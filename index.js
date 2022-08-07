ee274559d3ee4d0dd164d2b00cb15dd9e5e586d1
Keelan Daye <keelan.daye@gmail.com>
2022-08-07 20:44:07 +0100

    Add JSDoc

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-module.exports.removePowered = (req, res, next) => {
+ /**
+  * Removes X-Powered-By header as Express middleware
+  * 
+  * @param {Request} req 
+  * @param {Response} res 
+  * @param {NextFunction} next 
+  */
+const removePowered = (req, res, next) => {
 	res.removeHeader('x-powered-by');
 	next();
 };
+
+module.exports.removePowered = removePowered;
